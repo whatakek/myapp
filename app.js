88f62c07c550436245e8347a40a3c70dd874319f
Kirill <soapinbox@inbox.ru>
2022-10-26 09:27:54 +0300

    add proxy_pass

--- app.js
+++ app.js
@@ -1,68 +1,85 @@
 import express from 'express';
-require('dotenv').load();
+import dotenv from 'dotenv';
+import axios from 'axios';
+import multer from 'multer';
+const upload = multer({ dest: 'uploads/' })
+dotenv.config({path:'./keys.env'});
 const app = express();
-const jsonParser = express.json();
-const WEBSITE_HOST = keys.env.WEBSITE_HOST;
-const REAL_1C_HOST = keys.env.REAL_1C_HOST;
-let PROXY_PASS = keys.env.PROXY_PASS;
+const WEBSITE_HOST = process.env.WEBSITE_HOST;
+const REAL_1C_HOST = process.env.REAL_1C_HOST;
+let PROXY_PASS;
 const router = express.Router();
 
 app.get('/', (req, res) => {
-  res.send('status: ' + res.statusCode + '<br/>Hello World!');
+   res.send('status: ' + res.statusCode + '<br/>Hello World!');
 })
 
 //Получение дебиторской задолженности
-app.get('/user/debtInfo?userId=(<integer>)?', (req, res) => {
-  res.json({
-      debt: Number,
-      paydate: "20.04.2022",
-      overdebt: Number,
-      paysum: Number
-  })
+app.get('/user/debtInfo', (req, res) => {
+  if(PROXY_PASS){
+    const req1C = app.get(REAL_1C_HOST);
+    res.send(req1C)
+  }
+  else{res.json({
+      debt: 4,
+      paydate: "",
+      overdebt: 8,
+      paysum: 15
+  })}
 })
 
 //Создание заказа 
-//Здесь должен быть get запрос, который отправит пользователю форму или иным образом получит данные, которые мы затем распарсим
-router.post('/', jsonParser, (req, res, next) => {
+router.post('/', upload.none(), (req, res) => {
+  if(PROXY_PASS){
+    const req1C = app.get(REAL_1C_HOST);
+    res.send(req1C)
+  }
+  else{
   if(!req.body) return res.sendStatus(400);
-  res.json({orderId: '<integer>',
-            clientId: '<integer>',
-            items: {
-              code: Number,
-              pkg: Number,
-              price: Number,
-              quantity: Number,
-              'comment?': String
-  }});
-  res.send(res.statusCode);
-  if (res.statusCode === 200) {
-    app.listen(WEBSITE_HOST);
-    next()
-  };
-})
-
-router.put('/:id', jsonParser, (req,res) => {
-  res.json({
-  orderId: '<integer>',
-  items: {
-    code: Number,
-    pkg: Number,
-    price: Number,
-    quantity: Number,
-    'comment?': String,
-  }});
+  res.send({orderId: 1,
+            clientId: 10,
+            items: [{
+              code: 16,
+              pkg: 23,
+              price: 42,
+              quantity: 4,
+              comment: ''
+}]});
+  res.sendStatus(200);
+  axios.post('/', {orderId: 1,
+                  clientId: 10,
+                  items: [{
+                    code: 16,
+                    pkg: 23,
+                    price: 42,
+                    quantity: 4,
+                    comment: ''
+  }]});
+  res.sendStatus(200);
+}
 })
 
 //Получение моего ассортимента
-router.get('/recommended?userId=<integer>', (req, res) => {
-  res.json({
-      "<integer>": integer
-  })
+router.get('/recommended', (req, res) => {
+  if(PROXY_PASS){
+    const req1C = app.get(REAL_1C_HOST);
+    res.send(req1C)
+  }
+  else{
+  const recObj = {[req.query.userId] : 100}
+  res.json(recObj)
+  }
 })
 
 app.use('/order', router)
 
 //Получение акта сверки
-app.get('/reconciliation?userId=<integer>&start=<"20220701">&end=<"20220801">', (req, res) => {
-  res.download(`./${filename}.pdf`)
+app.get('/reconciliation', (req, res) => {
+  if(PROXY_PASS){
+    const req1C = app.get(REAL_1C_HOST);
+    res.send(req1C)
+  }
+  else{res.download('./6.pdf')}
 })
+
+app.listen(WEBSITE_HOST)
